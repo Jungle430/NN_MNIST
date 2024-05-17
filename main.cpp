0f54c32eb2752c3270e36be3664902c48c39e816
Jungle <1239946358@qq.com>
2024-05-17 15:14:32 +0800

    spdlog

--- main.cpp
+++ main.cpp
@@ -3,17 +3,17 @@
 #include "include/AsyncFileReader.h"
 #include "include/MNIST.hpp"
 #include "include/NeuralNetwork.hpp"
+#include "spdlog/spdlog.h"
 
 auto main() -> int {
-  NeuralNetwork<long double, 4> neural_network(
-      MNIST::DOMAIN, {{NN::DEFAULT_NODE_SIZE, "Sigmoid"},
-                      {NN::DEFAULT_NODE_SIZE, "Sigmoid"},
-                      {NN::DEFAULT_NODE_SIZE, "Sigmoid"},
+  NeuralNetwork<long double, 2> neural_network(
+      MNIST::DOMAIN, {{10, "Sigmoid"},
+                      
                       {MNIST::NUMBER_SIZE, "Sigmoid"}});
 
   MNIST::testNeuralNetwork(neural_network);
 
-  std::cout << "begin training..." << std::endl;
+  spdlog::info("begin training...");
 
   for (std::size_t i = 0; i < MNIST::DEFAULT_TRAIN_COUNT; i++) {
     std::cout << "The " << i + 1 << "st, train" << std::endl;
